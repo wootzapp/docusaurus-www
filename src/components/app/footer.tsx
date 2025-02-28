150d27b9bd5953158e1399cbddb5bb503086a72a
Sandeep Srinivasa <sss@wootzapp.com>
2025-02-28 20:39:24 +0530

    Update footer.tsx

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
       <div className="container flex flex-col md:flex-row justify-between gap-8 flex-wrap">
         <div className="flex flex-col gap-4">
           <img
-            {useBaseUrl('img/logo-footer.png')}
+            src={useBaseUrl('img/logo-footer.png')}
             alt="WootzApp Logo"
             className="h-14 w-52 max-md:h-10 max-md:w-44"
           />
@@ -25,7 +25,7 @@ export function Footer() {
             href="mailto:founders@wootzapp.com"
             className="flex gap-2 text-brand-orange-200 hover:underline ~text-xs/base items-center"
           >
-            <img {useBaseUrl('img/sms.png')} alt="Email Icon" className="w-6 h-6" />
+            <img src={useBaseUrl('img/sms.png')} alt="Email Icon" className="w-6 h-6" />
             founders@wootzapp.com
           </a>
         </div>
